c02b4863bae27f376e9e30c1ac62247750960009
Antti Ajanki <antti.ajanki@iki.fi>
2022-12-19 10:03:48 +0200

    Call the backend

--- editor-app/src/components/editor.tsx
+++ editor-app/src/components/editor.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import ReactQuill, { Quill } from 'react-quill';
+import React from 'react';
+import ReactQuill, { Quill, UnprivilegedEditor } from 'react-quill';
+import { DeltaStatic } from 'quill';
 import 'react-quill/dist/quill.snow.css';
 
 let Inline = Quill.import('blots/inline');
@@ -8,23 +9,47 @@ AnnotatedBlot.blotName = 'annotate';
 AnnotatedBlot.tagName = 'mark';
 Quill.register('formats/annotate', AnnotatedBlot);
 
-export function AnnotatingEditor() {
-  const [value, setValue] = useState('');
+interface IProps {
+};
 
-  const modules = {
-    toolbar: [
-        ['bold', 'italic', 'underline'],
-        [{'list': 'ordered'}, {'list': 'bullet'}]
-    ]
-  };
-  const formats = [
-    'bold', 'font', 'italic', 'annotate', 'link', 'size', 'underline', 'indent',
-    'list', 'direction'];
-
-  return <ReactQuill
-    theme='snow'
-    modules={modules}
-    formats={formats}
-    value={value}
-    onChange={setValue} />;
+interface IState {
+    content?: string;
 }
+
+export class AnnotatingEditor extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            content: ''
+        };
+    }
+
+    onChangeHandler = async (content: string, delta: DeltaStatic, source: string, editor: UnprivilegedEditor) => {
+        const response = await fetch("http://localhost:8000/api/hello")
+        const helloResponse = await response.json();
+        console.log(`on change API response: ${JSON.stringify(helloResponse)}`)
+
+        this.setState({
+            content: content
+        });
+    }
+
+    render() {
+        const modules = {
+            toolbar: [
+                ['bold', 'italic', 'underline'],
+                [{'list': 'ordered'}, {'list': 'bullet'}]
+            ]
+        };
+        const formats = [
+            'bold', 'font', 'italic', 'annotate', 'link', 'size', 'underline', 'indent',
+            'list', 'direction'];
+
+        return <ReactQuill
+            theme='snow'
+            modules={modules}
+            formats={formats}
+            value={this.state.content}
+            onChange={this.onChangeHandler} />;
+      }
+};
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from annotate import TextAnnotator
 
 app = FastAPI()
 annotator = TextAnnotator()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 class InputText(BaseModel):
@@ -16,8 +30,13 @@ async def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/api/hello")
+async def hello():
+    return {'message': 'moi'}
+
+
 @app.post("/api/annotate")
-async def read_item(inp: InputText):
+async def annotate(inp: InputText):
     return annotator.analyze(inp.text)
 
 
